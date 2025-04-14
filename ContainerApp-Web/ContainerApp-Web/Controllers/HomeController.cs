d97c7cbe4adf47fcba32399c374170ff2c76c9e7
Björn Vuokivi <bjorn.vuokivi@nexergroup.com>
2025-04-14 13:13:42 +0200

    Added web app

---
+++ ContainerApp-Web/ContainerApp-Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using ContainerApp_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainerApp_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+
+            TempData["ApplicationName"] = _configuration["MyConfig:ApplicationName"];
+            TempData["Version"] = _configuration["MyConfig:Version"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
